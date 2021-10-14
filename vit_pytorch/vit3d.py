a033eae5f6ad1c609c06b762371cc43ca3930662
william ashbee <wsashbee@gmail.com>
2021-10-13 22:06:14 -0400

    vit3d possibly implemented

--- vit_pytorch/vit3d.py
+++ vit_pytorch/vit3d.py
@@ -81,16 +81,17 @@ class ViT3d(nn.Module):
     def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
         super().__init__()
         image_height, image_width = pair(image_size)
+        image_depth = image_size
         patch_height, patch_width = pair(patch_size)
-
+        patch_depth = patch_size
         assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
 
-        num_patches = (image_height // patch_height) * (image_width // patch_width)
-        patch_dim = channels * patch_height * patch_width
+        num_patches = (image_height // patch_height) * (image_width // patch_width) * (image_depth // patch_depth)
+        patch_dim = channels * patch_height * patch_width * patch_depth
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            Rearrange('b c (h p1) (w p2) (z p3) -> b (h w z) (p1 p2 p3 c)', p1 = patch_height, p2 = patch_width, p3 = patch_depth),
             nn.Linear(patch_dim, dim),
         )
 
